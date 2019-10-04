ac1b34248f35eac9df674e2f3abc71bfdc08b347
Steve Mullins <sgmullins@gmail.com>
2019-10-04 14:34:41 -0600

    Add user login/logout

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { postRegister } = require('../controllers/index');
+const { postRegister, postLogin, getLogout } = require('../controllers/index');
 const { errorHandler } = require('../middleware/index');
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -22,9 +23,10 @@ router.get('/login', (req, res, next) => {
 });
 
 /* POST /login  */
-router.post('/login', (req, res, next) => {
-  res.send('POST /login');
-});
+router.post('/login', postLogin);
+
+/* GET /logout  */
+router.get('/logout', getLogout);
 
 /* GET /profile  */
 router.get('/profile', (req, res, next) => {
