0f783be25aa6060da8d4b26af5de484a018c520f
Steve Mullins <sgmullins@gmail.com>
2019-10-07 09:10:46 -0600

    add post destroy route, finish CRUD routing for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { asyncErrorHandler } = require('../middleware');
-const { postIndex, postNew, postCreate, postShow, postEdit, postUpdate } = require('../controllers/posts');
+const { postIndex, postNew, postCreate, postShow, postEdit, postUpdate, postDestroy } = require('../controllers/posts');
 
 /* GET posts index /posts */
 router.get('/', asyncErrorHandler(postIndex));
@@ -23,8 +23,6 @@ router.get('/:id/edit', asyncErrorHandler(postEdit));
 router.put('/:id', asyncErrorHandler(postUpdate));
 
 /* DELETE posts destroy /posts/:id */
-router.delete('/:id', (req, res, next) => {
-    res.send('DELETE /posts/:id');
-  });
+router.delete('/:id', asyncErrorHandler(postDestroy));
 
 module.exports = router;
