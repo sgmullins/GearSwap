4783518ede241aa649de38cda8a04b0748a8af4a
Steve Mullins <sgmullins@gmail.com>
2019-10-14 08:48:37 -0600

    add pagination package, seed.js and seed the posts page

---
+++ seeds.js
@@ -0,0 +1,20 @@
+const faker = require("faker");
+const Post = require('./models/post');
+
+async function seedPosts(){
+    await Post.remove();
+    for(const i of new Array(40)){
+        const post = {
+            title: faker.lorem.word(),
+            description: faker.lorem.text(),
+            author: {
+                '_id' : '5d9cc82f579e2240b85edeb0',
+                'username' : 'steve'
+            }
+        }
+        await Post.create(post);
+    }
+    console.log('40 new posts created');
+}
+
+module.exports = seedPosts;
