1a08792c30ef16d8981a6201565d964963a99283
Steve Mullins <sgmullins@gmail.com>
2019-11-14 09:10:08 -0700

    add bootstrapcdn, begin code for searchFilter form

--- models/post.js
+++ models/post.js
@@ -1,66 +1,68 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review = require('./review');
-const mongoosePaginate = require('mongoose-paginate');
+const Review = require("./review");
+const mongoosePaginate = require("mongoose-paginate");
 
 const PostSchema = new Schema({
-    title: String,
-    price: String,
-    description: String,
-    images: [{ url: String, public_id: String }],
-    location: String,
-    geometry: {
-        type: {
-          type: String, 
-          enum: ['Point'],
-          required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-          }
-      },
-    properties:  {
-        description: String
+  title: String,
+  price: String,
+  description: String,
+  images: [{ url: String, public_id: String }],
+  location: String,
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true
     },
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Review"
+    coordinates: {
+      type: [Number],
+      required: true
     }
-],
-    avgRating: { type: Number, default: 0 }
+  },
+  properties: {
+    description: String
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ],
+  avgRating: { type: Number, default: 0 }
 });
 
-PostSchema.pre('remove', async function(){
-    await Review.remove({
-        _id: {
-            $in: this.reviews
-        }
-    });
+PostSchema.pre("remove", async function() {
+  await Review.remove({
+    _id: {
+      $in: this.reviews
+    }
+  });
 });
 
 //add instance method for average rating
 PostSchema.methods.calculateAvgRating = function() {
-    let ratingsTotal = 0;
-    if(this.reviews.length) {
-        this.reviews.forEach(review => {
-            ratingsTotal += review.rating;
-        });
-        this.avgRating = Math.round((ratingsTotal / this.reviews.length) * 10) / 10;
-    } else {
-        this.avgRating = ratingsTotal;
-    }
-    
-    const floorRating = Math.floor(this.avgRating);
-    this.save();
-    return floorRating;
-}
+  let ratingsTotal = 0;
+  if (this.reviews.length) {
+    this.reviews.forEach(review => {
+      ratingsTotal += review.rating;
+    });
+    this.avgRating = Math.round((ratingsTotal / this.reviews.length) * 10) / 10;
+  } else {
+    this.avgRating = ratingsTotal;
+  }
+
+  const floorRating = Math.floor(this.avgRating);
+  this.save();
+  return floorRating;
+};
 
 PostSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model('Post', PostSchema);
+PostSchema.index({ geometry: "2dsphere" });
+
+module.exports = mongoose.model("Post", PostSchema);
--- routes/index.js
+++ routes/index.js
@@ -15,63 +15,59 @@ const {
   postLogin,
   getLogout,
   getProfile,
-  updateProfile
+  updateProfile,
+  getForgotPw,
+  putForgotPw,
+  getReset,
+  putReset
 } = require('../controllers');
 const {
   asyncErrorHandler,
   isLoggedIn,
   isValidPassword,
   changePassword
-} = require('../middleware');
-
+} = require('../middleware')
 
 /* GET home/landing page. */
 router.get('/', asyncErrorHandler(landingPage));
 
-/* GET /register  */
+/* GET /register */
 router.get('/register', getRegister);
 
-/* POST /register  */
+/* POST /register */
 router.post('/register', upload.single('image'), asyncErrorHandler(postRegister));
 
-/* GET /login  */
+/* GET /login */
 router.get('/login', getLogin);
 
-/* POST /login  */
+/* POST /login */
 router.post('/login', asyncErrorHandler(postLogin));
 
-/* GET /logout  */
+/* GET /logout */
 router.get('/logout', getLogout);
 
-/* GET /profile  */
+/* GET /profile */
 router.get('/profile', isLoggedIn, asyncErrorHandler(getProfile));
 
-/* PUT /profile  */
-router.put('/profile', isLoggedIn,
+/* PUT /profile */
+router.put('/profile',
+  isLoggedIn,
   upload.single('image'),
   asyncErrorHandler(isValidPassword),
   asyncErrorHandler(changePassword),
   asyncErrorHandler(updateProfile)
 );
 
-/* GET /forgot  */
-router.get('/forgot', (req, res, next) => {
-  res.send('GET /forgot');
-});
+/* GET /forgot */
+router.get('/forgot-password', getForgotPw);
 
-/* PUT /forgot  */
-router.put('/forgot', (req, res, next) => {
-  res.send('PUT /forgot');
-});
+/* PUT /forgot */
+router.put('/forgot-password', asyncErrorHandler(putForgotPw));
 
-/* GET /reset/:token  */
-router.get('/reset/:token', (req, res, next) => {
-  res.send('GET /reset/:token');
-});
+/* GET /reset/:token */
+router.get('/reset/:token', asyncErrorHandler(getReset));
 
-/* PUT /reset/:token  */
-router.put('/reset/:token', (req, res, next) => {
-  res.send('PUT /reset/:token');
-});
+/* PUT /reset/:token */
+router.put('/reset/:token', asyncErrorHandler(putReset));
 
 module.exports = router;
